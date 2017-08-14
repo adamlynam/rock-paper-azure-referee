9f3cd045a8f8151e9596629ced29397ff26943ea
Adam <adam@lynam.co.nz>
2017-08-14 23:56:31 +0100

    Updating move generators

--- src/main/scala/model/GameMove.scala
+++ src/main/scala/model/GameMove.scala
@@ -5,7 +5,26 @@ import scala.util.Random
 object GameMove extends Enumeration {
   val ROCK, PAPER, SCISSORS, DYNAMITE, WATERBOMB = Value
 
-  def randomMove = {
+  def randomMove: GameMove.Value = {
     values.toVector((new Random).nextInt(values.size))
+  }
+
+  def probabalisticDynamiteMove: GameMove.Value = {
+    if (GameState.getRemainingDynamite > 0 && GameState.getRemainingTurns > 0 && (new Random).nextDouble < GameState.getRemainingDynamite / GameState.getRemainingTurns) {
+      GameMove.DYNAMITE
+    }
+    else {
+      val remainingMoves = List(GameMove.ROCK, GameMove.PAPER, GameMove.SCISSORS)
+      remainingMoves.toVector((new Random).nextInt(remainingMoves.size))
+    }
+  }
+
+  def copyOpponentMove: GameMove.Value = {
+    if (GameState.opponentHistory.length > 0) {
+      GameState.opponentHistory.last
+    }
+    else {
+      values.toVector((new Random).nextInt(values.size))
+    }
   }
 }
